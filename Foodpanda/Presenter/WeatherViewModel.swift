b6ecdef4dd173c2e6886a3b80009239dd655f3a9
Judy Tsai <judy.tsai@vpon.com>
2024-06-25 21:55:14 +0800

    Implement WeatherViewModel

--- Foodpanda/Presenter/WeatherViewModel.swift
+++ Foodpanda/Presenter/WeatherViewModel.swift
@@ -13,14 +13,14 @@ enum WeatherViewModelError: Error {
 
 class WeatherViewModel {
     
-    let useCase: WeatherUseCase
+    lazy var useCase: WeatherUseCase = makeUseCase()
     
     var weatherDidUpdate: ((Weather) -> Void)?
     var errorDidUpdate: ((WeatherViewModelError) -> Void)?
     
-    init(useCase: WeatherUseCase) {
-        self.useCase = useCase
-    }
+//    init(useCase: WeatherUseCase) {
+//        self.useCase = useCase
+//    }
     
     func loadTaiwanWeather() {
         useCase.loadWeather(fromCountry: "taiwan", OnDate: "20240625") { result in
@@ -33,5 +33,14 @@ class WeatherViewModel {
                 self.errorDidUpdate?(WeatherViewModelError.viewModelError)
             }
         }
+    }
+}
+
+private extension WeatherViewModel {
+    func makeUseCase() -> WeatherUseCase {
+        let client = URLSessionHTTPClient(session: .shared)
+        let remoteRepo = WeatherRemoteRepository(client: client)
+        let useCase = WeatherUseCase.init(remoteRepo: remoteRepo)
+        return useCase
     }
 }
