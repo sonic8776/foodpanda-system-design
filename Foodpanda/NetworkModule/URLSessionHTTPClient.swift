7de16ad94e39d425173c05a6bec7ba216b1a8024
Judy Tsai <judy.tsai@vpon.com>
2024-06-25 21:54:00 +0800

    Implement URLSessionHTTPClient

---
+++ Foodpanda/NetworkModule/URLSessionHTTPClient.swift
@@ -0,0 +1,36 @@
+//
+//  URLSessionHTTPClient.swift
+//  Foodpanda
+//
+//  Created by Judy Tsai on 2024/6/25.
+//
+
+import Foundation
+
+class URLSessionHTTPClient: HTTPClient {
+    
+    let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func request(withRequestType requestType: RequestType, completion: @escaping (Result<(Data, HTTPURLResponse), HTTPClientError>) -> Void) {
+        
+        session.dataTask(with: requestType.urlRequest) { data, response, error in
+            if let error {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  let data,
+                  response.statusCode == 200 else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            completion(.success((data, response)))
+        }
+    }
+}
