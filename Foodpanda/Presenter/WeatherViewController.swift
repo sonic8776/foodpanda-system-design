b9980d64b0744d1c86b0d0e789d92d01d0b4bf7c
Judy Tsai <sonic8776@gmail.com>
2024-06-26 10:35:19 +0800

    Implement WeatherViewController

---
+++ Foodpanda/Presenter/WeatherViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherViewController.swift
+//  Foodpanda
+//
+//  Created by Judy Tsai on 2024/6/25.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    
+    lazy var temperatureLabel = makeLabel()
+    lazy var humidityLabel = makeLabel()
+    let viewModel = WeatherViewModel()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        viewModel.weatherDidUpdate = { [weak self] weather in
+            self?.temperatureLabel.text = String(weather.temperature)
+            self?.humidityLabel.text = String(weather.humidity)
+        }
+        
+        viewModel.errorDidUpdate = { error in
+            // error handling
+        }
+    }
+    
+    func setupUI() {
+        
+    }
+}
+
+private extension WeatherViewController {
+    func makeLabel() -> UILabel {
+        let label = UILabel()
+        return label
+    }
+    
+    
+}
